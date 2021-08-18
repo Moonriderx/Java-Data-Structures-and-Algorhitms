56d475af5d745bf6b83c582256e17642e2a61ed3
Moonriderx <Luben1993@abv.bg>
2021-08-18 13:22:25 +0300

    Examples Java

---
+++ src/Loop.java
@@ -0,0 +1,35 @@
+public class Loop {
+
+    /*public void print(int[] numbers) {
+        0(n)
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+
+        }
+    }
+
+    public void print(int[] numbers) {
+        0(2+n)
+        System.out.println(); //0(1)
+        for (int i = 0; i < numbers.length; i++) { //0(n)
+            //System.out.println(numbers[i]);
+        }
+        System.out.println();//0(1)
+    }
+
+
+    public void print(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+        System.out.println(numbers[i]);
+    }
+
+    for (int i = 0; i < numbers.length; i++) {
+        System.out.println(numbers[i]);
+    }
+    }
+
+     */
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+        public void print(int[] numbers) {
+            // 0(1)
+            System.out.println(numbers[0]);
+            System.out.println(numbers[0]);
+        }
+
+}
